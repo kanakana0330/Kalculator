b96b90047c61246f2bfddf8f9a870f6313a2c4c3
kanakana0330 <kanakana0330@gmail.com>
2013-05-03 16:05:44 +0900

    (+/-)を押した直後のバグを解消

--- kalculator.js
+++ kalculator.js
@@ -31,31 +31,39 @@
     //0:四則計算ボタンを押てない　1:四則計算ボタンを押した
     operation = '',
     //計算結果
-    result = ''
+    result = '',
     //1:equalを押したとき
-    equal_flg = 0;
+    equal_flg = 0,
     //0:数字を連続して入力する　1:上書き
-    number_flg = 0;
-
-    //モニターの幅を取得
-    max = 15;
-    //max = parseInt((monitor_numbers.offsetWidth - 20) / 11);
+    number_flg = 0,
+    // (+/-)を押したとき
+    posiNega_flg = 0,
+    //最大桁数
+    max = 15
   ;
 
-  //1〜9の数字を押したときのメソッド
+  /*
+  *1〜9の数字を押したときのメソッド
+  */
   function inputBasic(arg){
-    if(equal_flg == 1){
+    //イコールを押した直後の処理
+    if(equal_flg === 1){
       clearAll();
     }
-    if(number_flg !== 0){
+    // (+/-)を押した直後は数字を上書きする
+    if(posiNega_flg === 1){
+      number_flg = 1;
+    }
+    //number_flgが0なら連続入力1なら上書きする
+    if(number_flg !== 0){ //上書き処理
       number_displayed = arg;
       number_flg = 0;
-    } else {
-      //number_displayedが0なら上書き
-      if (number_displayed !== '0') {
+      posiNega_flg = 0;
+    } else { //連続入力
+      if (number_displayed !== '0') {//number_displayedが0以外なら連続入力
         number_displayed += arg;
         number_flg = 0;
-      }else {
+      }else {//number_displayedが0なら上書き
         number_displayed = arg;
         number_flg = 0;
       }
@@ -63,57 +71,82 @@
     returnNumber();
   }
 
-  //0を押したときのメソッド
+  /*
+  *0を押したときのメソッド
+  */
   function inputZero(arg){
+    //イコールを押した直後の処理
     if(equal_flg == 1){
       clearAll();
     }
+    // (+/-)を押した直後は数字を上書きする
+    if(posiNega_flg === 1){
+      number_displayed = arg;
+      returnNumber();
+      posiNega_flg = 0;
+    }
+    //number_displayedが0以外の場合に連続入力
     if(number_displayed !== '0'){
       number_displayed += arg;
       returnNumber();
     }
   }
 
-  //dotを押したときのメソッド
+  /*
+  *dotを押したときのメソッド
+  */
   function inputDot(arg){
+    //イコールを押した直後の処理
     if(equal_flg == 1){
       clearAll();
     }
-    if(number_displayed !== '' && number_displayed.indexOf('.') == -1){
-      if(number_displayed !== '.'){
-        number_displayed += arg;
-        returnNumber();
-        number_flg = 0;
+    // (+/-)を押した直後は何もしない
+    if(posiNega_flg !== 1){
+      //number_displayedに既に'.'がない場合'.'を入れる
+      if(number_displayed !== '' && number_displayed.indexOf('.') == -1){
+        //number_displayedの先頭が'.'にならないようにする
+        if(number_displayed !== '.'){
+          number_displayed += arg;
+          returnNumber();
+          number_flg = 0;
+        }
       }
     }
   }
 
-  // (+/-)を押したときのメソッド
+  /*
+  * (+/-)を押したときのメソッド
+  */
   function input_posiNega(){
-  if(equal_flg == 1){
-    clearAll();
-    number_displayed = result;
-    returnNumber();
+    //イコールを押した直後の処理
+    if(equal_flg == 1){
+      clearAll();
+      number_displayed = result;
+      returnNumber();
     }
-
+    // (+/-)を切り替えるてキャッシュする
     if(number_displayed !== ''){
       number_displayed = String(number_displayed);
       if(number_displayed.charAt(0) !== '-'){
         number_displayed = '-' + number_displayed
       }　else {
-        number_displayed = number_displayed.slice(1);     
+        number_displayed = number_displayed.slice(1);
       }
+      returnNumber();
+      posiNega_flg = 1;
     }
-    returnNumber();
   }
 
-  //clear button を押したときのメソッド
+  /*
+  * clearを押したときのメソッド
+  */
   function input_clear(){
     clearAll();
   }
 
-
-  //equal button を押したときのメソッド
+  /*
+  * equalを押したときのメソッド
+  */
   function input_equal(){
     if(number_displayed !== '' && operation !== ''){
       number_cache.push(parseFloat(number_displayed));
@@ -123,91 +156,35 @@
     }
   }
 
-  //plus button を押したときのメソッド
-  function input_plus(){
-    if(equal_flg == 1){
-      number_flg = 1;
-      monitor_functions.innerHTML = "+";
-      equal_flg = 0;
-      operation = "+";
-    } else {
-      if(number_displayed !== ''){
+  /*
+  * 四則計算ボタンを押したときのメソッド
+  */
+  function arithmetic(arg){
+    var mark = arg;
+   // if(posiNega_flg !== 1){
+      if(equal_flg == 1){
         number_flg = 1;
-        monitor_functions.innerHTML = "+";
-        number_cache.push(parseFloat(number_displayed));
-        number_displayed = '';
-        if(operation !== ''){
-          calculation();
+        monitor_functions.innerHTML = mark;
+        equal_flg = 0;
+        operation = mark;
+      } else {
+        if(number_displayed !== ''){
+          number_flg = 1;
+          monitor_functions.innerHTML = mark;
+          number_cache.push(parseFloat(number_displayed));
+          number_displayed = '';
+          if(operation !== ''){
+            calculation();
+          }
+          operation = mark;
         }
-        operation = "+";
       }
-    }
+   // }
   }
 
-  //minus button を押したときのメソッド
-  function input_minus(){
-    if(equal_flg == 1){
-      number_flg = 1;
-      monitor_functions.innerHTML = "-";
-      equal_flg = 0;
-      operation = "-";
-    } else {
-      if(number_displayed !== ''){
-        number_flg = 1;
-        monitor_functions.innerHTML = "-";
-        number_cache.push(parseFloat(number_displayed));
-        number_displayed = '';
-        if(operation !== ''){
-          calculation();
-        }
-        operation = "-";
-      }
-    }
-  }
-
-  //division button を押したときのメソッド
-  function input_division(){
-    if(equal_flg == 1){
-      number_flg = 1;
-      monitor_functions.innerHTML = "/";
-      equal_flg = 0;
-      operation = "/";
-    } else {
-      if(number_displayed !== ''){
-        number_flg = 1;
-        monitor_functions.innerHTML = "/";
-        number_cache.push(parseFloat(number_displayed));
-        number_displayed = '';
-        if(operation !== ''){
-          calculation();
-        }
-        operation = "/";
-      }
-    }
-  }
-
-  //multiply button を押したときのメソッド
-  function input_multiply(){
-    if(equal_flg == 1){
-      number_flg = 1;
-      monitor_functions.innerHTML = "*";
-      equal_flg = 0;
-      operation = "*";
-    } else {
-      if(number_displayed !== ''){
-        number_flg = 1;
-        monitor_functions.innerHTML = "*";
-        number_cache.push(parseFloat(number_displayed));
-        number_displayed = '';
-        if(operation !== ''){
-          calculation();
-        }
-        operation = "*";
-      }
-    }
-  }
-
-  //計算メソッド
+  /*
+  * 計算メソッド
+  */
   function calculation(){
     if(operation === '+'){
       result = number_cache[0] + number_cache[1];
@@ -221,20 +198,26 @@
       monitor_numbers.innerHTML = result;
       number_cache.splice(0,2);
       number_cache.push(parseFloat(result));
+      posiNega_flg = 0;
   }
 
-  //クリアメソッド
+  /*
+  * クリアメソッド
+  */
   function clearAll(){
     number_displayed = '';
     operation = '';
     number_flg = 0;
     equal_flg = 0;
+    posiNega_flg = 0;
     monitor_functions.innerHTML = operation;
     number_cache.splice(0,2);
     returnNumber();
   }
 
-  //数字を表示させるメソッド
+  /*
+  * 数字を表示させる（桁数制限あり）メソッド
+  */
   function returnNumber(){
     if(number_displayed.length < max){
       monitor_numbers.innerHTML = number_displayed;
@@ -242,11 +225,11 @@
     } else {
       monitor_numbers.style.background = '#ff0000'
     }
-    
-    
   }
 
-  //inputボタンを押したときの変数
+  /*
+  * ボタンを押したときの各変数
+  */
   function input_1(){inputBasic('1');}
   function input_2(){inputBasic('2');}
   function input_3(){inputBasic('3');}
@@ -259,7 +242,14 @@
   function input_0(){inputZero('0');}
   function input_dot(){inputDot('.');}
 
-  //イベント登録
+  function input_plus(){arithmetic('+');}
+  function input_minus(){arithmetic('-');}
+  function input_division(){arithmetic('/');}
+  function input_multiply(){arithmetic('*');}
+
+  /*
+  * イベント登録
+  */
   btn_1.addEventListener('click',input_1,false);
   btn_2.addEventListener('click',input_2,false);
   btn_3.addEventListener('click',input_3,false);
